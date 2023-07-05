711d6befaf18001eca0c84a28ab9c0717cdda308
PACKOJIbHUKOB <jordn-5@yandex.ru>
2023-07-05 12:16:22 +0300

    домашнее задание 12 выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+
 public class Main {
-    public static void main(String[] args) {
 
 
+    public static void main() {
+        Author lukyanenko = new Author("Сергей","Лукъяненко");
+        Book labirintOtrazeniy= new Book(lukyanenko.getAuthor(),"Лабиринт Отражений",1997 );
+        Author perumov = new Author("Ник","Перумов");
+        Book cherepNaRukove= new Book(perumov.getAuthor(),"Череп на Рукове",1997 );
+        cherepNaRukove.setAgeOfPublication(2002);
     }
 }
