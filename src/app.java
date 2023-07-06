974b819dffe9474a5b1d3ec820ec10acf8f9098e
PACKOJIbHUKOB <jordn-5@yandex.ru>
2023-07-06 14:00:00 +0300

    домашнее задание 13 выполнено

--- src/app.java
+++ src/app.java
@@ -3,10 +3,14 @@ public class app {
     public static void main(String[] args) {
          Author lukyanenko = new Author("Сергей","Лукъяненко");
          Book labirintOtrazeniy= new Book(lukyanenko,"Лабиринт Отражений",1997 );
+         Book prozrachnieVitrazi= new Book(lukyanenko,"Прозрачные витаражи",1999 );
          Author perumov = new Author("Ник","Перумов");
          Book cherepNaRukove= new Book(perumov,"Череп на Рукове",1997 );
          cherepNaRukove.setAgeOfPublication(2002);
-        System.out.printf("автор книги :%s\nНазвание книги: %s\nГод издания: %s\n",labirintOtrazeniy.getAuthor(),labirintOtrazeniy.getTitleBook(),labirintOtrazeniy.getAgeOfPublication());
-        System.out.printf("автор книги :%s\nНазвание книги: %s\nГод издания: %s\n",cherepNaRukove.getAuthor(),cherepNaRukove.getTitleBook(),cherepNaRukove.getAgeOfPublication());
+        System.out.println(labirintOtrazeniy.getAuthor()+labirintOtrazeniy.getTitleBook()+labirintOtrazeniy.getAgeOfPublication()+"hashcode: "+labirintOtrazeniy.hashCode());
+        System.out.println(prozrachnieVitrazi.getAuthor()+prozrachnieVitrazi.getTitleBook()+prozrachnieVitrazi.getAgeOfPublication()+"hashcode: "+prozrachnieVitrazi.hashCode());
+        System.out.println(cherepNaRukove.getAuthor()+cherepNaRukove.getTitleBook()+cherepNaRukove.getAgeOfPublication()+"hashcode: "+cherepNaRukove.hashCode());
+        System.out.println(labirintOtrazeniy.equals(prozrachnieVitrazi));
+        System.out.println(labirintOtrazeniy.equals(cherepNaRukove));
     }
 }
